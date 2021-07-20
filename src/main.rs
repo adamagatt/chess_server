804d1ec842b6d8b0cebf1b4e8c1814d3ede92bf9
Adam Gatt <adam.gatt@live.com.au>
2021-07-20 17:12:37 +0930

    Use HTTP Response codes

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,10 @@ mod game;
 mod render;
 mod storage;
 
+use rocket::http::Status;
+use storage::op::StorageError;
+use storage::models::Game;
+
 #[launch]
 fn rocket() -> _ {
     rocket::build().mount("/", routes![
@@ -16,25 +20,27 @@ fn rocket() -> _ {
 }
 
 #[get("/")]
-fn index() -> String {
-    render::to_text(chess::initial_board(), render::get_space_map())
+fn index() -> (Status, String) {
+    (Status::Ok, render::to_text(chess::initial_board(), render::get_space_map()))
 }
 
 #[post("/game")]
-fn create_new_game() -> String {
+fn create_new_game() -> (Status, String) {
     game::create_new_game().map_or_else(
-        |err: storage::op::StorageError| err.to_string(),
-        |new_game_code: String| new_game_code
+        |_err: StorageError| (Status::InternalServerError, String::from("")),
+        |new_game_code: String| (Status::Created, new_game_code)
     )
 }
 
 #[get("/game/<code>")]
-fn find_game_by_code(code: &str) -> String {
+fn find_game_by_code(code: &str) -> (Status, String) {
     game::load(code)
     .map_or_else(
-        |err: storage::op::StorageError| err.to_string(),
-        |found_game: storage::models::Game| {
-            found_game.code
-        }
+        |err: StorageError| match err {
+            StorageError::Query(err) if (err==diesel::NotFound)
+              => (Status::NoContent, String::from("")),
+            _ => (Status::InternalServerError, String::from(""))
+        },
+        |found_game: Game| (Status::Ok, found_game.code)
     )
 }
